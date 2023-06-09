9cf837724d08f7ed79ab18eb3e45e6448cb586bc
Curt Sapp <dev@curts.app>
2023-06-09 14:51:27 -0700

    Added social media metadata

--- components/metadata.js
+++ components/metadata.js
@@ -4,11 +4,11 @@ export function Metadata(props) {
     let title = "Alan Acosta";
     let description = "Alan Acosta - jazz saxophonist - jazz flute - live. connect. about. media. discography. contact.";
 
-    if(props?.title) {
+    if (props?.title) {
         title = props.title;
     }
 
-    if(props?.description) {
+    if (props?.description) {
         description = props.description;
     }
 
@@ -18,5 +18,14 @@ export function Metadata(props) {
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:title" content="Alan Acosta" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://alanacosta.com/press/IMG_0540.jpg" />
+        <meta property="og:url" content="https://alanacosta.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:description" content="Learn more about upcoming events and connect with Alan." />
+        <meta property="og:site_name" content="Alan Acosta" />
+        <meta name="twitter:image:alt" content="Alan playing a saxophone surrounded by colorful feathers"></meta>
     </Head>
 }
